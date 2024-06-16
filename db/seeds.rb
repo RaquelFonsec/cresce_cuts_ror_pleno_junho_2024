aa74d47f527699aa5f83df98d7c00cd825744e32
Raquel Fonsec <raque-leto@hotmail.com>
2024-06-16 22:49:00 +0200

    Atualizados os seeds para garantir consistência nos dados de campanhas. Adicionados ícones visuais na página de listagem de campanhas para melhorar a experiência do usuário.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,6 @@
 require 'httparty'
 
-
 API_URL = 'https://fakestoreapi.com/products'
-
 
 def fetch_products
   response = HTTParty.get(API_URL)
@@ -15,14 +13,9 @@ def fetch_products
 end
 
 
-CampaignHistory.destroy_all
-Discount.destroy_all
-Campaign.destroy_all
 Product.destroy_all
 
-
 products = fetch_products
-
 
 products.each_with_index do |product_data, index|
   product = Product.create(
@@ -30,24 +23,51 @@ products.each_with_index do |product_data, index|
     price: product_data['price'].to_f
   )
 
-  campaign = Campaign.create(
-    title: "Campanha #{index + 1}",
-    description: "Promoção de #{rand(5..30)}% de desconto!",
+  
+  percentual_discount_value = rand(5..30)
+  percentual_discounted_price = product.price * (1 - percentual_discount_value.to_f / 100)
+
+  percentual_campaign = Campaign.create(
+    title: "Campanha Percentual #{index + 1}",
+    description: "Promoção de #{percentual_discount_value}% de desconto!",
     start_date: Date.today,
     end_date: Date.today + 7.days,
-    discounted_price: product.price * (1 - rand(5..30).to_f / 100),
+    discounted_price: percentual_discounted_price,
     product: product,
     status: 'ativo',
     created_by: 'admin'
   )
 
   Discount.create(
-    campaign: campaign,
+    campaign: percentual_campaign,
     discount_type: 'percentual',
-    discount_value: rand(5..30)
+    discount_value: percentual_discount_value
   )
 
-  puts "Criada campanha: #{campaign.title} - Desconto: #{campaign.discount.discount_value}%"
+  puts "Criada campanha percentual: #{percentual_campaign.title} - Desconto: #{percentual_campaign.discount.discount_value}%"
+
+  
+  fix_discount_value = rand(5..15).to_f
+  fix_discounted_price = product.price - fix_discount_value
+
+  fix_campaign = Campaign.create(
+    title: "Campanha Fixa #{index + 1}",
+    description: "Promoção de R$ #{fix_discount_value} de desconto!",
+    start_date: Date.today,
+    end_date: Date.today + 7.days,
+    discounted_price: fix_discounted_price,
+    product: product,
+    status: 'ativo',
+    created_by: 'admin'
+  )
+
+  Discount.create(
+    campaign: fix_campaign,
+    discount_type: 'fixo',
+    discount_value: fix_discount_value
+  )
+
+  puts "Criada campanha fixa: #{fix_campaign.title} - Desconto: R$ #{fix_campaign.discount.discount_value}"
 end
 
-puts "Seed de campanhas com desconto percentual concluído!"
+puts "Seed de campanhas com desconto percentual e fixo concluído!"
