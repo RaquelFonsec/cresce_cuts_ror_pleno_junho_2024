a58751499f67662a57cf60f02c103011ba7dc53d
Raquel Fonsec <raque-leto@hotmail.com>
2024-06-17 22:51:11 +0200

    Adiciona teste RSpec para verificar a funcionalidade de edição de desconto em Campanha
    Inclui teste que verifica se o método calculate_discounted_price em Campanha atualiza corretamente o preço com desconto após a edição do desconto. Corrige a atribuição do desconto em uma instância válida de Campanha para evitar o erro de método indefinido. Teste bem-sucedido para validação da funcionalidade de edição do desconto.

--- spec/models/campaign_spec.rb
+++ spec/models/campaign_spec.rb
@@ -29,5 +29,22 @@ RSpec.describe Campaign, type: :model do
         expect(campaign.discounted_price).to eq(200.0)
       end
     end
-  end
-end
+    
+    context 'quando o desconto é editado' do
+      it 'atualiza o preço com desconto corretamente após a edição do desconto' do
+        
+        campaign = Campaign.create(product: product)
+        desconto_original = Discount.create(discount_type: 'percentual', discount_value: 25.0, campaign: campaign)
+        campaign.discount = desconto_original
+        campaign.calculate_discounted_price
+        expect(campaign.discounted_price).to eq(150.0)
+    
+       
+        desconto_editado = Discount.create(discount_type: 'percentual', discount_value: 50.0, campaign: campaign)
+        campaign.discount = desconto_editado
+        campaign.calculate_discounted_price
+        expect(campaign.discounted_price).to eq(100.0)
+      end
+    end
+  end 
+end     
